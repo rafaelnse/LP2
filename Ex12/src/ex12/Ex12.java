ccf20786b614bc654f5a14e6c08d4f74df0e3653
Rafael Neves <108910159+rafaelnse@users.noreply.github.com>
2025-04-10 00:26:18 -0300

    Update Ex12.java

--- Ex12/src/ex12/Ex12.java
+++ Ex12/src/ex12/Ex12.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Ex12 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
-        int a, b;
+        int a, b, soma=0, total=0;
+        float media;
         
         System.out.printf("Digite um numero para o intervalo: ");
         a = entrada.nextInt();
@@ -14,10 +14,26 @@ public class Ex12 {
         
         System.out.println("Numeros pares dentro do intervalo: ");
         
-        for (int i=a; i <= b; i++){
-            if (i%2==0){
-                System.out.printf("%d, ", i);
+        if(b>a){
+            for (int i=a; i <= b; i++){
+                if (i%2==0){
+                    System.out.printf("%d, ", i);
+                    soma += i;
+                    total++;
+                }
             }
+            media = soma/total;
+        
+        System.out.println("");
+        System.out.printf("Total da soma dos pares eh: %d\n", total);
+        System.out.printf("Media dos numeros pares eh: %.2f\n", media);
+        }
+        else{
+            for (int i=b; i <= a; i++){
+                if (i%2==0){
+                    System.out.printf("%d, ", i);
+                }
+            }            
         }
     }
 }
